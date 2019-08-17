10069ca22fa7818f03ff201b2e1dd00c8ad56274
Emmanuel <emmanuel.barbin@gmail.com>
2019-08-17 11:51:27 -0300

    pug impl

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
+//const path = require('path');
+//const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    console.log(adminData.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop', {prods: products, docTitle: 'My Shop'});
 });
 
 module.exports = router;
