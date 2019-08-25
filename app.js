9f52e153ac2c828278d6de0d85175cb9edba978b
ebarbin <emmanuel.barbin@gmail.com>
2019-08-24 21:41:46 -0300

    mas cosas

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -10,6 +12,13 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    User.findByPk(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+});
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,8 +28,28 @@ app.use('/admin/', adminRoutes);
 app.use('/', shopRoutes);
 app.use(errorControllers.get404);
 
-sequelize.sync().then(result => {
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product)
+
+sequelize
+    //.sync({force: true})
+    .sync()
+    .then(result => {
+
+        return User.findByPk(1);
+    
+}).then(user => {
+
+    if (!user) 
+        return User.create({name: 'Emma', email: 'emmanuel.barbin@gmail.com'});
+    else
+        return new Promise((resolve, reject)=> { resolve(user); });
+
+}).then((user) => {
 
     app.listen(3000);
-});
+})
+.catch(err => {
+    console.log(err);
+})
 
